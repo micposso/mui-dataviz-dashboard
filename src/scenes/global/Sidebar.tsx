5c6c7ea036b7dba0eef9a253ad09f6c08794ecf4
micposso <micposso@gmail.com>
2024-01-13 16:09:29 -0500

    set up sidebar state changes

--- src/scenes/global/Sidebar.tsx
+++ src/scenes/global/Sidebar.tsx
@@ -1,38 +1,84 @@
 import { useState } from "react";
-import { Sidebar as ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import {
+  Sidebar as ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+} from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
-import {
-  LightModeOutlined as LightModeOutlinedIcon,
-  DarkModeOutlined as DarkModeOutlinedIcon,
-  NotificationsOutlined as NotificationsOutlinedIcon,
-  SettingsOutlined as SettingsOutlinedIcon,
-  PersonOutlined as PersonOutlinedIcon,
-  Search as SearchIcon,
-} from "@mui/icons-material";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 const Sidebar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const [ollapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
   const [selected, setSelected] = useState("Dashboard");
 
   return (
     <Box mb="25px">
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <img
-          src=""
-            style={{ cursor: "pointer", borderRadius: "50%" }}
-          alt="profile"
-        />
-      </Box>
-      <Box textAlign="center">
-        <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m: '20px'}}>User Name</Typography>
-        <Typography variant="h3">User Name</Typography>
-      </Box>
+      <ProSidebar collapsed={collapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setCollapsed(!collapsed)}
+            icon={collapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!collapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+          {/* profile and header */}
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <img
+              src=""
+              style={{ cursor: "pointer", borderRadius: "50%" }}
+              alt="profile"
+            />
+          </Box>
+          <Box textAlign="center">
+            <Typography
+              variant="h2"
+              color={colors.grey[100]}
+              fontWeight="bold"
+              sx={{ m: "20px" }}
+            >
+              User Name
+            </Typography>
+            <Typography variant="h3">User Name</Typography>
+          </Box>
+        </Menu>
+      </ProSidebar>
     </Box>
   );
-}
+};
 
 export default Sidebar;
