b134097b2fa449d6c938ce1b519b6ab3d7d3dd18
micposso <micposso@gmail.com>
2024-01-24 19:28:01 -0500

    build header component

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,5 +1,12 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
 const DashBoard = () => {
-  return (<h1>DashBoard</h1>)
+  return <Box>
+    <Box>
+      <Header title="DashBoard" subtitle="Welcome to your Dashboard" />
+    </Box>
+  </Box>
 };
 
 export default DashBoard;
